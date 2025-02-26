a228a1515dacee4faed5ca0b0e6a9b0459043b08
Chinedu Winner <chineduwinnerchibundo@gmail.com>
2025-02-26 13:57:21 +0100

    deplot it

--- src/app/material/material.component.ts
+++ src/app/material/material.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon'
 import {MatCardModule} from '@angular/material/card';
@@ -7,14 +7,44 @@ import {MatInput, MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule, FormControl, ReactiveFormsModule} from '@angular/forms';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { Dialog } from '@angular/cdk/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
+import {MatSnackBar} from '@angular/material/snack-bar'
+import { SnackbarComponent } from '../snackbar/snackbar.component';
+import {MatBadgeModule} from '@angular/material/badge';
 
 
 @Component({
   selector: 'app-material',
-  imports: [MatButtonModule, MatIconModule, MatCardModule, MatChipsModule, MatFormFieldModule, FormsModule, MatInputModule, MatAutocompleteModule, ReactiveFormsModule ],
+  imports: [MatButtonModule, MatIconModule, MatCardModule, MatChipsModule, MatFormFieldModule, FormsModule, MatInputModule, MatAutocompleteModule, ReactiveFormsModule, MatDialogModule, MatBadgeModule ],
   templateUrl: './material.component.html',
   styleUrl: './material.component.css'
 })
 export class MaterialComponent {
-  mycontrol = new FormControl() 
+  mycontrol = new FormControl('')
+  
+  dialog = inject(MatDialog)
+
+  snackbar = inject(MatSnackBar)
+
+  openDialog(enterAnimationDuration:string, exitAnimationDuration: string, message:string){
+    this.dialog.open(DialogComponent,{
+      width:'400px',
+      enterAnimationDuration,
+      exitAnimationDuration, 
+      data:{
+        dialog_message: message
+      }
+    })
+  }
+
+  openSnackbar(){
+    this.snackbar.openFromComponent(SnackbarComponent,{
+      duration: 5000,
+      horizontalPosition: 'left',
+      verticalPosition: 'top', 
+    })
+  }
+
 }
